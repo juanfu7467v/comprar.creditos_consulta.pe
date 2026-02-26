9448ab0a3e8abf50525608f054f66ae10e535f26
juanfu7467v <movistarcervise@gmail.com>
2026-02-25 21:41:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ const cspDomains = [
   "'self'",
   "https://masitaprex.com",
   "https://auth.masitaprex.com",
+  "https://peliprex.fly.dev",
   "https://lh3.googleusercontent.com",
   "https://peliprex-pe-v2.fly.dev",
   "https://1.bp.blogspot.com",
