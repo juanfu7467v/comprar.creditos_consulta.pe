26d7c504f2a6e268a627f82b62f9e38a40f1c9cc
juanfu7467v <movistarcervise@gmail.com>
2026-03-25 17:03:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import app from "./src/server.js";
 import logger from "./src/utils/logger.js";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   logger.info("SERVER", `Servidor iniciado en el puerto ${PORT}`);
